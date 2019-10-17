5b8ae43c5ac670b91d13036eea5f70ff2beb28a4
Paulo Cezar <Paulo Cezar@INSPIRON1545>
2019-10-16 21:11:00 -0300

    Sprint Teste

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'NOME: Paulo Cezar Alves de Oliveira';
+        echo '<br>';
+        echo 'MATRICULA: 18114290041'
+        ?>
+    </body>
+</html>
